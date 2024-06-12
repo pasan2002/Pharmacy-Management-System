0a19a245ac069f64c04563babc29bc964f820a48
Pasan Hewavitharana <115849043+pasan2002@users.noreply.github.com>
2024-06-12 18:43:42 +0530

    initial commit

---
+++ PMS/src/application/Main.java
@@ -0,0 +1,54 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	
+	private double x = 0;
+	private double y = 0;
+	
+	
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("Scene.fxml"));
+			Scene scene = new Scene(root);
+			
+			root.setOnMousePressed((MouseEvent event) -> {
+				x = event.getSceneX();
+				y = event.getSceneY();
+			});
+			
+			root.setOnMouseDragged((MouseEvent event) -> {
+				primaryStage.setX(event.getScreenX() - x);
+				primaryStage.setY(event.getScreenY() - y);
+				
+				primaryStage.setOpacity(.8);
+			});
+			
+			root.setOnMouseReleased((MouseEvent event) -> {
+				primaryStage.setOpacity(1);
+			});
+			
+			primaryStage.initStyle(StageStyle.TRANSPARENT);
+			
+			scene.getStylesheets().add(getClass().getResource("loginDesign.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
